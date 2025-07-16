ff759f9fcd8d601e57a81175a0ad7dc7aedfcea3
SamuelClucas <samuelclucas2001@gmail.com>
2025-07-16 17:14:04 +0100

    Device features, device, and queue all setup, contained in App :p

--- src/backend_admin/app_with_event_handler.rs
+++ src/backend_admin/app_with_event_handler.rs
@@ -1,12 +1,12 @@
 use std::default;
-
+use tokio::*;
 use wgpu::wgc::present::SurfaceOutput;
 use winit::application::ApplicationHandler;
 use winit::event::{WindowEvent};
 use winit::event_loop::{ActiveEventLoop};
 use winit::window::{Window, WindowId};
 use wgpu::{Instance, InstanceDescriptor, RequestAdapterOptionsBase};
-use wgpu::{RequestAdapterOptions, PowerPreference, Surface} ;
+use wgpu::{RequestAdapterOptions, PowerPreference, Surface, DeviceDescriptor, Device, Queue, Features, FeaturesWGPU, FeaturesWebGPU, Limits, MemoryHints, Trace} ;
 
 /// Setup for logical App struct, with logical Window \n
 /// App implements ApplicationHandler for resuming of app, WindowEvent handling \n
@@ -15,28 +15,54 @@ use wgpu::{RequestAdapterOptions, PowerPreference, Surface} ;
 #[derive(Default)]
 pub struct App {
     window: Option<Window>,
+   device: Option<Device>,
+    queue: Option<Queue>,
 }
 
 /// implements ApplicationHandler for logical App
 impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+    #[tokio::main] // this is for async! see here: https://rust-lang.github.io/async-book/part-guide/async-await.html
+    async fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window_attributes = Window::default_attributes()
                 .with_title("📦")
                 .with_blur(true);
         self.window = Some(event_loop.create_window(window_attributes).unwrap());
 
-        
         let inst_descriptor = InstanceDescriptor::from_env_or_default();
         let graphics_instance = Instance::new(&inst_descriptor);
 
         let surface = graphics_instance.create_surface(self.window.as_ref().unwrap()).ok(); // used to wrap window
+        
         let request_adapter_options = RequestAdapterOptions { // see here for an explanation of each option :) https://gpuweb.github.io/gpuweb/#dictdef-gpurequestadapteroptions
             power_preference: PowerPreference::None,
             force_fallback_adapter: true,
             compatible_surface: surface.as_ref()
         };
+        let adapter_wan = (graphics_instance.request_adapter(&request_adapter_options)).await.unwrap(); // logical *handle* for physical GPU!
         
-        let adapter = graphics_instance.request_adapter(&request_adapter_options);
+        // device descriptor and request (device, queue)
+        let desc = DeviceDescriptor {
+            required_features: Features {features_wgpu: {FeaturesWGPU::POLYGON_MODE_POINT; 
+                                                        FeaturesWGPU::STORAGE_RESOURCE_BINDING_ARRAY; 
+                                                        FeaturesWGPU::BUFFER_BINDING_ARRAY;
+                                                        FeaturesWGPU::MULTI_DRAW_INDIRECT;
+                                                        FeaturesWGPU::PUSH_CONSTANTS;
+                                                        FeaturesWGPU::POLYGON_MODE_LINE},
+                                                        features_webgpu: {FeaturesWebGPU::DEPTH_CLIP_CONTROL}}, // only request what you require (see about OptionalCapabilities - limits and features - here: https://gpuweb.github.io/gpuweb/#feature also https://gpuweb.github.io/gpuweb/#feature-index)
+            required_limits: Limits::downlevel_defaults(),
+            trace: Trace::Off,
+            memory_hints: MemoryHints::Performance,
+            label: Some("wan adapter")
+
+        }; // see here for dev desc debrief: https://gpuweb.github.io/gpuweb/#dictdef-gpudevicedescriptor
+        let (device, queue) = adapter_wan.request_device(&desc).await.unwrap(); 
+        self.device = Some(device);
+        self.queue = Some(queue);
+        // Device is an open connection to your GPU. Adapter can now die, it's okay :')
+        // Dev's gonna be responsible for all the really cool stuff :p
+        // TODO: enclose this backend setup within a conditional, use a bool flag in app to prevent redoing all of it on successive resumptions
+
+
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
