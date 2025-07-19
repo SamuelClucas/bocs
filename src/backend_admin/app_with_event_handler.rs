60950f2de9545ed904ea11947957d4b32ec0f4e1
SamuelClucas <samuelclucas2001@gmail.com>
2025-07-19 11:51:08 +0100

    Added dpi scaling updates (display changes for example); window configuration ready; create surface function to be written for recreation of surface after device request await (instace, surface configuration, window all stored in App { .. })

--- src/backend_admin/app_with_event_handler.rs
+++ src/backend_admin/app_with_event_handler.rs
@@ -1,11 +1,17 @@
+use std::num::NonZeroU32;
 use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
 use winit::event::{WindowEvent};
 use winit::event_loop::{ActiveEventLoop, EventLoopProxy};
 use winit::window::{Window, WindowId};
 use wgpu::{Instance, InstanceDescriptor};
 use wgpu::{RequestAdapterOptions, PowerPreference, DeviceDescriptor, Device, Queue, Features, FeaturesWGPU, FeaturesWebGPU, Limits, MemoryHints, Trace} ;
 use tokio::{spawn};
-use wgpu::{CommandEncoderDescriptor, RenderPassDescriptor, RenderPassColorAttachment, Operations, LoadOp, StoreOp};
+use wgpu::{Surface, CommandEncoderDescriptor, 
+    RenderPassDescriptor, 
+    RenderPassColorAttachment, 
+    Operations, LoadOp, StoreOp,
+    SurfaceConfiguration,TextureUsages, CompositeAlphaMode};
 
 /// Used to match user event proxies in App::user_event
 pub enum Outcome {
@@ -18,18 +24,24 @@ pub enum Outcome {
 #[derive(Default)]
 pub struct App {
     setup: bool,
+    graphics_instance: Option<Instance>,
+    surface_configuation: Option<SurfaceConfiguration>,
     proxy: Option<EventLoopProxy<Outcome>>,
-    window: Option<Window>, 
+    window: Option<Window>,
+    scale_factor: Option<f64>,
    device: Option<Device>,
     queue: Option<Queue>,
 }
 
-impl  App  {
+impl App  {
     pub fn new (fun: impl FnOnce()-> EventLoopProxy<Outcome>) -> App {
-        App  {
+        App {
             setup: true,
-            proxy:  Some(fun()), // smuggle proxy into app for downstream requests
+            graphics_instance: None,
+            surface_configuation: None,
+            proxy:  Some(fun()), // smuggle proxy into app using move closure for downstream requests
             window: None,
+            scale_factor: None,
             device: None,
             queue: None
         }
@@ -39,20 +51,36 @@ impl  App  {
 impl ApplicationHandler<Outcome> for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) { // ran once on init
         if self.setup {
+            // window: logical representation of gpu output, managed by system os
             let window_attributes = Window::default_attributes()
                     .with_title("📦")
                     .with_blur(true);
             self.window = Some(event_loop.create_window(window_attributes).expect("\x1b[1;31mError creating window\x1b[0m\n"));
-            // neither of these worked    let inst_descriptor = InstanceDescriptor::from_env_or_default().with_env();
-            //let inst_descriptor = InstanceDescriptor::with_env(self);
-            let inst_descriptor = InstanceDescriptor { backends: (wgpu::Backends::METAL), ..Default::default()};
-            let graphics_instance = Instance::new(&inst_descriptor);
+            self.scale_factor = Some(self.window.as_ref().unwrap().scale_factor());
+            // Infos for surface config 
+            let window_size = self.window.as_ref().unwrap().inner_size();
+       
+            // This line is untested!!
+            let inst_descriptor = InstanceDescriptor { backends: (wgpu::Backends::METAL), ..InstanceDescriptor::from_env_or_default()}; 
+            self.graphics_instance = Some(Instance::new(&inst_descriptor));
+            let surface = Some(self.graphics_instance.as_ref().unwrap().create_surface(self.window.as_ref().unwrap()).expect("\x1b[1;32mError creating surface\x1b[0m\n")); // used to wrap window
+            // TODO: create surface function using instance, window, and surface configuration
+            self.surface_configuation = Some(SurfaceConfiguration {
+                width: NonZeroU32::new(window_size.width).unwrap().into(),
+                height: NonZeroU32::new(window_size.height).unwrap().into(),
+                usage: TextureUsages::RENDER_ATTACHMENT, // can be used as render pass output
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                present_mode: wgpu::PresentMode::Fifo, // vsync! cool right?!
+                desired_maximum_frame_latency: 2,
+                view_formats: vec![wgpu::TextureFormat::Bgra8UnormSrgb;0],
+                alpha_mode: CompositeAlphaMode::Opaque // later change for transparency
+            });
+            
 
-            let surf = graphics_instance.create_surface(self.window.as_ref().unwrap()).expect("\x1b[1;32mError creating surface\x1b[0m\n"); // used to wrap window
             let request_adapter_options = RequestAdapterOptions { // see here for an explanation of each option :) https://gpuweb.github.io/gpuweb/#dictdef-gpurequestadapteroptions
                 power_preference: PowerPreference::None,
                 force_fallback_adapter: false,
-                compatible_surface: Some(&surf)
+                compatible_surface: Some(self.surface.as_ref().unwrap())
             };
             
             // device descriptor and request (device, queue)
@@ -65,7 +93,7 @@ impl ApplicationHandler<Outcome> for App {
                 label: Some("wan adapter")
 
             }; // see here for dev desc debrief: https://gpuweb.github.io/gpuweb/#dictdef-gpudevicedescriptor
-            let adapter_request = graphics_instance.request_adapter(&request_adapter_options);
+            let adapter_request = self.graphics_instance.as_ref().unwrap().request_adapter(&request_adapter_options);
             
             //  cannot call await in App due to ApplicationHandler constraint 
             // for this reason I will spawn a task using a tokio runtime, use a user event proxy to get dev and queue into app
@@ -131,6 +159,23 @@ impl ApplicationHandler<Outcome> for App {
             WindowEvent::CloseRequested => {
                 println!("Slipping through my fingers all the time\nI try to capture every minute... \nThe feeling in it, slipping through my fingers all the time... \nUntil next time!");
                 event_loop.exit();
+            },
+            WindowEvent::ScaleFactorChanged { .. } => {
+                // Store scale factor for dynamic DPI-aware resizing
+                self.scale_factor = Some(self.window.as_ref().unwrap().scale_factor());
+            },
+            
+            WindowEvent::Resized(phys) => {
+                if let (Some(scale), Some(config)) = (self.scale_factor, self.surface_configuation.as_mut()) {
+                    config.width = ((phys.width as f64) * scale) as u32; // conversion to logical size
+                    config.height = ((phys.height as f64) * scale) as u32; 
+                } else {
+                    eprintln!("Warning: Missing scale factor or surface config during resize.");
+                }
+            
+                if let Some(window) = self.window.as_ref() {
+                    window.request_redraw();
+                }
             },
             WindowEvent::RedrawRequested => {
                 // Redraw the application.
