9025fd4a36fcb5e92525485d995a5e9119c242f9
SamuelClucas <samuelclucas2001@gmail.com>
2025-08-03 12:51:58 +0100

    Armed with the determinant, cross and dot products, I now have camera basis code with associated cross and dot functions :)

--- src/backend_admin/camera.rs
+++ src/backend_admin/camera.rs
@@ -2,58 +2,58 @@ use cgmath::{Vector2, Vector3, Vector4};
 use winit::dpi::PhysicalSize;
 
 
-pub struct Orbital_Camera {
-    pos_from_origin: Vector3<f32>, // negate for from camera
-    origin_from_pos: Vector3<f32>,
-    frustum: [Vector3<f32>; 4],
-    fov: f32, // could be int but simpler to implement with same type
-    radius: f32
+pub struct OrbitalCamera {
+    c: Vector3<f32>, // where c is camera pos in world space, 
+    f: Vector3<f32>, // where f is unit vector from c to world space origin, orthogonal to u and r (u X r)
+    u: Vector3<f32>, // where u is unit vector up from c, orthogonal to f and r (f x r)
+    r: Vector3<f32>, // where r is unit vector right from c, orthogonal to f and u (f X u)
     
     
 }
 
-impl Orbital_Camera {   
-    pub fn new(fov: f32, radius: f32, window_size: PhysicalSize<u32>) -> Self {
-        let mut pitch: f32 = 0.0;
-        let mut yaw: f32 = 0.0;
-        // vector f rom origin to camera
-        let mut pos: Vector3<f32> = Vector3::<f32>{
-            x: pitch.cos() * yaw.sin() * radius, 
-            y: pitch.sin() * radius, 
-            z: pitch.cos() * yaw.cos() * radius};
+impl OrbitalCamera {   
+    // returns right-handed, orthogonal vector to a, b
+    pub fn cross(a: &Vector3<f32>, b: &Vector3<f32>) -> Vector3<f32>{
+        Vector3::new(
+            (a.y * b.z) - (a.z * b.y), // x
+            (a.z * b.x) - (a.x * b.z), // y
+            (a.x * b.y) - (a.y * b.x) // z
+        )
+    }
+    // returns scalar sum of component-wise products of a and b
+    pub fn dot(a: &Vector3<f32>, b: &Vector3<f32>) -> f32{
+        (a.x*b.x)+(a.y*b.y)+(a.z*b.z)
+    }
 
-        // vector from camera to origin 
-        let mut inverse: Vector3<f32> = Vector3::<f32>{
-            x: -(pitch.cos() * yaw.sin()), // pitch increase, x decrease; yaw increase, x increase
-            y: -(pitch.sin()), 
-            z: -(pitch.cos() * yaw.cos())
-        };
-        // horizontal fov delta
-        let delta_in_radians = fov.to_radians() / 2.0;
+    pub fn magnitude(input: &Vector3<f32>) -> f32 {
+        let square = Self::dot(input, input);
+        square.sqrt()
+    }
 
-        let frustum = [ // bottom left to bottom right, ccw
-            Vector3::<f32> {
-                x: -delta_in_radians.cos(),
-                y: -delta_in_radian.cos(),
+    pub fn new(window_size: PhysicalSize<u32>, i: f32, j: f32, k: f32) -> Self {
+        let pos = Vector3::new(i,j,k);
+        let mag = Self::magnitude(&pos);
+        // forward is negative camera pos, normalised by its magnitude
+        let forward = -pos/mag;
 
-            },
-            Vector3::<f32> {
+        // at first, let up be 1 unit in j direction
+        let up = Vector3::new(pos.x, pos.y + 1.0, pos.z);
 
-            },
-            Vector3::<f32> {
+        // get right by u cross f, then normalise by its magnitude
+        let right = Self::cross(&up, &forward);
+        let right = right/Self::magnitude(&right); // norm
 
-            },
-            Vector3::<f32> {
+        // recompute up to ensure orthogonality with forward and right in new basis
+        // this is done by forward cross right, then normalise (just to be safe)
+        let up = Self::cross(&forward, &right); // now truly orthogonal to f and r
+        let up = up/Self::magnitude(&up); // norm
 
-            }
-        ];
-
-        
-        Camera {
-            pos_from_origin: pos,
-            origin_from_pos: inverse,
-            radius: radius,
-        }
+       OrbitalCamera { 
+        c: pos,
+        f: forward, 
+        u: up, 
+        r: right 
+    }
 
     }
     
